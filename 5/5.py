c0a58463c63b130c170efead9f66aa550b5577d7
Jake Madison <jakemadison@gmail.com>
2013-12-05 16:13:34 -0800

    start 5.

--- 5/5.py
+++ 5/5.py
@@ -3,8 +3,26 @@
 #
 # What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
-
 #testing each number for div by 1-20 seems a waste. if something is divisible by 4 & 5m then it's divisible by 20 too.
+# i guess all prime factors of 20 would be the way to go?
+
+
+#gimmie a number... iterate up to that number yielding primes
+def primes(n):
+    yield 1
+    yield 2
+    yield 3
+    x = 4
+    while x < n:
+
+
+
+
+
+
+
+
+
 
 start = 1
 r = 10
@@ -25,8 +43,6 @@ def main():
     while rng > 0:
         is_divisible(2520, rng)
         rng -= 1
-
-
 
 
 main()
