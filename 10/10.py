a849e532c91ce1cea801c2bb2b78ea8b058335a3
Jake Madison <jakemadison@gmail.com>
2013-12-09 12:54:05 -0800

    ew, that is ugly. (but it works!)

--- 10/10.py
+++ 10/10.py
@@ -1,1 +1,7 @@
 #stubb for project:  10
+# The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+#
+# Find the sum of all the primes below two million.
+
+# pretty sure this is a good one for our primes generator again.
+
--- 9/9.py
+++ 9/9.py
@@ -12,6 +12,7 @@
 
 # I can't believe this ugly garbage actually worked!!!
 
+
 def is_series(a, b, c):
     if a < b < c:
         return True
@@ -25,26 +26,6 @@ def is_triplet(a, b, c):
     else:
         return False
 
-
-
-#okay, so a only ever has to iterate up to total/3 (actually even less than that)
-# 12. 10, 1, 1. 9, 2, 1. 8,3,1. 7,4,1. 7,3,2
-# right, so essentially, this has to iterate down, recursively branching to get our list
-# of a,b,cs that we can then test for is_triplet.
-
-
-
-tt = 12
-a = 1
-b = 2
-c = 3
-
-# find all permutations where a+b+c = tt, but a < b < c is true.
-
-# while True:
-#     if is_triplet(a, b, c):
-#         break
-#     else:
 
 def get_list(num):
     def break_number(n):
@@ -85,30 +66,3 @@ def get_list(num):
 
 
 get_list(1000)
-
-#
-# print is_triplet(1,2,3)
-# print is_triplet(3,4,5)
-# print is_triplet(4,5,6)
-#
-#
-# max_total = 1000
-#
-#
-#
-# # i could find all of the a < b < c's that are under tt, then test if they are pythagorean triplets.
-#
-#
-# a = 3
-# b = 4
-#
-# c = int(math.sqrt((a ** 2) + (b ** 2)))
-#
-# temptotal = a + b + c
-# print 'temptotal: ', temptotal
-#
-# print c
-#
-# c = 5
-#
-#
